5bcbc67adbc60ba2b691158d9efa4ac1698d4d54
nivetha2809 <nivethaj2809@gmail.com>
2025-07-02 14:55:33 +0530

    marks

---
+++ src/com/marks.java
@@ -0,0 +1,11 @@
+public class marks {
+    public static void main(String[] args) {
+        int [] marks  = {80, 99, 67, 90, 89};
+        for(int mark : marks){
+            System.out.println(mark);
+
+        }
+
+    }
+    
+}
