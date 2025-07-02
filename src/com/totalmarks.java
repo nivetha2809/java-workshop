1c276c3386f02d401b643f64c9af59b35a2c9e77
nivetha2809 <nivethaj2809@gmail.com>
2025-07-02 15:31:24 +0530

    totalmarks

---
+++ src/com/totalmarks.java
@@ -0,0 +1,17 @@
+public class totalmarks {
+    public static void main(String[] args) {
+        int [] marks  = {80, 99, 67, 90, 89};
+        int total = 0;
+
+        for(int mark : marks){
+            total += mark;
+
+        }
+        System.out.println("total marks:"+total);
+
+    }
+    
+}
+
+    
+
