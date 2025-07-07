82fe0ad3132e5235fc40e12cfe21e50fe9efc37d
nivetha2809 <nivethaj2809@gmail.com>
2025-07-07 10:30:38 +0530

    Stringexample.java

--- src/com/vetias/workshop/basic/Stringexample.java
+++ src/com/vetias/workshop/basic/Stringexample.java
@@ -1,4 +1,4 @@
-public class StringExample {
+public class Stringexample {
     public static void main(String[] args) {
         String FirstName = "Nivetha";
         String LastName = "jayagansan";
