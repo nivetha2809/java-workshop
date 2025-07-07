e6f6b4c1171b738742047c2b608a3f7977604491
nivetha2809 <nivethaj2809@gmail.com>
2025-07-07 10:49:22 +0530

    update

--- src/com/vetias/workshop/basic/Stringexample.java
+++ src/com/vetias/workshop/basic/Stringexample.java
@@ -1,14 +1,17 @@
-
-
-
-public class Stringexample {
+public class StringExample {
     public static void main(String[] args) {
-        String FirstName = "Sree";
-        String LastName = "Krishnan";
+        String FirstName = "nivetha";
+        String LastName = "jayaganesan";
         String FinalName = FirstName.concat(LastName);
         System.out.println("Concatenated String: " + FinalName);
         String UpperCase = FinalName.toUpperCase();
         System.out.println("Uppercase String: " +UpperCase);
+
+
+        String sentence = "this program shows how can we split a string into multiple string";
+        String words[] = sentence.split("");
+        System.out.println(words.length);
+
         
         
     
