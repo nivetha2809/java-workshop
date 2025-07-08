25986bbc41a90affd80af62e697ab838b9c87755
nivetha2809 <nivethaj2809@gmail.com>
2025-07-08 14:46:48 +0530

    calc

---
+++ src/com/vetias/workshop/basic/Caluculator.java
@@ -0,0 +1,25 @@
+public class Caluculator {
+   public static void main(String []args) {
+       int a=6,b=0;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        try{
+            if (b==0) 
+            {
+                throw new ArithmeticException("0 cant be a divisor");
+
+                
+            }
+        }catch(ArithmeticException e){
+                System.out.println(e.getMessage());
+        }
+            
+    }
+
+}
+
+
+    
+
+
